14956dcef8a6cb68943922889e1e45879f9753ee
ztx <zhaotianxion@gmail.com>
2023-12-06 14:27:37 +0800

    fix import error

--- app/modules/index.ts
+++ app/modules/index.ts
@@ -1,4 +1,4 @@
-import ReactEpubViewer from 'modules/reactViewer/ReactViewer';
-import EpubViewer from 'modules/epubViewer/EpubViewer';
+import ReactEpubViewer from "../modules/reactViewer/ReactViewer";
+import EpubViewer from "../modules/epubViewer/EpubViewer";
 
 export { EpubViewer, ReactEpubViewer };
