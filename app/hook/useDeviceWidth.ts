6cd7b267cf40e2460e047ea31f77a2382428a27b
ztx <zhaotianxion@gmail.com>
2023-12-15 11:34:45 +0800

    feat: add the response ui

---
+++ app/hook/useDeviceWidth.ts
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+const useDeviceWidth = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return screenWidth;
+};
+
+export default useDeviceWidth;
